bba0e4d6e18a2e470c408331921c90c29a7f0c90
nlightnlabs <@nlightnlabs.com>
2024-11-08 20:37:39 -0800

    commit

--- src/functions/storeSessionData.js
+++
@@ -1,7 +0,0 @@
-
-export const storeSessionData = (stateData)=>{
-    Object.keys(stateData).map(key=>{
-        localStorage.setItem(key, stateData[key]); // Store a key-value pair
-    })
-}
-
--- src/redux/actions.js
+++
@@ -1,5 +0,0 @@
-export const CLEAR_STORE = 'CLEAR_STORE';
-
-export const clearStore = () => ({
-  type: CLEAR_STORE,
-});
